28645377180e39a4325a2078936e6848d78ceb3b
thara0402 <thara0402@gmail.com>
2018-06-21 01:22:40 +0900

    update az-run.sh

--- az-run.sh
+++ az-run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -it --rm -v ${HOME}/.kube:/root/.kube -v ${HOME}/develop:/develop thara0402/azure-cli-k8s:latest /develop/rabbit-house/azure-cli
+docker run -it --rm -v ${HOME}/.kube:/root/.kube -v ${HOME}/develop:/develop thara0402/azure-cli-k8s:latest
+#docker run -it --rm -v ${HOME}/.kube:/root/.kube -v ${HOME}/develop:/develop azuresdk/azure-cli-python:latest
